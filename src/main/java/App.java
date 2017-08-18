e91ea72ec1169088f36af5af4e599c1c027f93db
Victor Ngeno <ngenovictor321@gmail.com>
2017-08-18 15:01:35 +0300

    create the App.java file with root route and attempt to load squads

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,25 @@
 import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args){
-        get("/");
+        staticFileLocation("public");
+        String layout = "templates/layout.vtl";
+
+        get("/", (Request request, Response response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String title = "Home";
+            model.put("title",title);
+            model.put("squads",Squad.all());
+            model.put("template","templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
     }
 }
